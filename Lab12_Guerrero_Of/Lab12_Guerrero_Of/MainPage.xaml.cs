ab18880e69e346310c1eee983b556f203e346f88
Bronx972 <ricardo.guerrero@tecsup.edu.pe>
2023-06-16 23:49:50 -0500

    Agregar archivos de proyecto.

---
+++ Lab12_Guerrero_Of/Lab12_Guerrero_Of/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab12_Guerrero_Of
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
